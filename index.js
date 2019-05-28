762ce9cb36611f577904711bb194176145490505
Juan Carlos Romero <juancarlos.rmr@gmail.com>
2019-05-28 14:31:33 +0200

    implementa clase Sftp

--- index.js
+++ index.js
@@ -18,232 +18,265 @@ var normalizePath = function (path) {
     return path.replace(/\\/g, '/');
 };
 
-module.exports = function (options, callback) {
+var events = require('events');
 
-    return Q.Promise(function (resolve, reject) {
-        options = assign({
-            "host": "",
-            "port": "36000",
-            "user": "",
-            "pass": "",
-            "remotePath": "",
-            "sourcePath": "./",
-            "remotePlatform": "unix",
-            "includePattern": null,
-            "cacheFile": null
-        }, options);
 
-        if (options.host === undefined || options.host === '') {
-            throw new Error('sftp2', '`host` required.');
-        }
+/**
+ * Clase apra realizar despliegues por SFTP con usuario y contraseña
+ * 
+ */
+function SftpDeployer() {
 
-        var fileCount = 0;
-        var fileLength = 0;
+    var eventEmitter = new events.EventEmitter();
+    var silent = false;
 
-        options.password = options.password || options.pass;
-        options.username = options.username || options.user || 'anonymous';
+    var deployLog = function (data) {
+        if (!silent) process.stdout.write(data);
 
-        var remotePath = options.remotePath;
-        var sourcePath = options.sourcePath;
-        var remotePlatform = options.remotePlatform;
+    }
 
-        var mkDirCache = {};
+    this.on = function (eventName, callback) {
+        eventEmitter.on(eventName, callback);
+    }
 
-        var finished = false;
-        var connectionCache = null;
-        var cache = {},
-            cacheFilePath = null;
-        if (options.cacheFile) {
-            cacheFilePath = path.resolve(options.cacheFile);
-            try {
-                cache = require(cacheFilePath);
-            } catch (e) {
-                console.log("Archivo caché:", cacheFilePath, "inválido o no existe, se creará uno nuevo");
+
+    this.upload = function (options, callback) {
+
+        return Q.Promise(function (resolve, reject) {
+
+
+            options = assign({
+                "host": "",
+                "port": "36000",
+                "user": "",
+                "pass": "",
+                "remotePath": "",
+                "sourcePath": "./",
+                "remotePlatform": "unix",
+                "includePattern": null,
+                "cacheFile": null,
+                "silent": false
+            }, options);
+
+            if (options.host === undefined || options.host === '') {
+                throw new Error('sftp2', '`host` requerido.');
             }
-        }
 
-        var items = [];
-        fs.walk(sourcePath)
-            .on('data', function (item) {
-                if (!item.stats.isDirectory()) {
-                    if (!options.includePattern || item.path.match(options.includePattern)) {
-                        items.push(item);
-                    }
+            var fileCount = 0;
+            var fileLength = 0;
+
+            options.password = options.password || options.pass;
+            options.username = options.username || options.user || 'anonymous';
+
+            var remotePath = options.remotePath;
+            var sourcePath = options.sourcePath;
+            var remotePlatform = options.remotePlatform;
+            silent = options.silent;
+
+            var mkDirCache = {};
+
+            var finished = false;
+            var connectionCache = null;
+            var cache = {},
+                cacheFilePath = null;
+            if (options.cacheFile) {
+                cacheFilePath = path.resolve(options.cacheFile);
+                try {
+                    cache = require(cacheFilePath);
+                } catch (e) {
+                    deployLog("Archivo caché:", cacheFilePath, "inválido o no existe, se creará uno nuevo");
                 }
-            })
-            .on('end', function () {
-                fileLength = items.length;
+            }
 
-                if (fileLength <= 0) {
-                    console.log('sftp2:', chalk.yellow('no se han subido archivos'));
-                } else {
-                    if (options.sort) {
-                        items = items.sort(options.sort);
+            var items = [];
+            fs.walk(sourcePath)
+                .on('data', function (item) {
+                    if (!item.stats.isDirectory()) {
+                        if (!options.includePattern || item.path.match(options.includePattern)) {
+                            items.push(item);
+                        }
                     }
-                    return uploadFiles(items);
-                }
-            });
+                })
+                .on('end', function () {
+                    fileLength = items.length;
 
-        function uploadFiles(files) {
-
-            connectSftp(function (sftp) {
-                async.eachSeries(files, function (file, done) {
-                    var filepath = file.path.replace(/\\/, '/');
-
-                    var pathArr = sourcePath.replace(/\/$/, '').split('/');
-
-                    var projectName = pathArr[pathArr.length - 1];
-
-                    var relativePath = filepath.split(projectName + path.sep)[1];
-                    var finalRemotePath = normalizePath(path.join(remotePath, relativePath));
-
-                    var dirname = path.dirname(finalRemotePath);
-
-                    var fileDirs = parents(dirname)
-                        .map(function (d) {
-                            return d.replace(/^\/~/, "~");
-                        })
-                        .map(normalizePath);
-
-                    if (dirname.search(/^\//) === 0) {
-                        fileDirs = fileDirs.map(function (dir) {
-                            if (dir.search(/^\//) === 0) {
-                                return dir;
-                            }
-                            return '/' + dir;
-                        });
+                    if (fileLength <= 0) {
+                        deployLog('sftp2:', chalk.yellow('no se han subido archivos'));
+                    } else {
+                        if (options.sort) {
+                            items = items.sort(options.sort);
+                        }
+                        return uploadFiles(items);
                     }
+                });
 
-                    fileDirs = fileDirs.filter(function (d) {
-                        return d.length >= remotePath.length && !mkDirCache[d];
-                    });
+            function uploadFiles(files) {
 
-                    async.whilst(function () {
-                        return fileDirs && fileDirs.length;
-                    }, function (next) {
-                        var d = fileDirs.pop();
-                        mkDirCache[d] = true;
+                connectSftp(function (sftp) {
+                    async.eachSeries(files, function (file, done) {
+                        var filepath = file.path.replace(/\\/, '/');
 
-                        if (remotePlatform && remotePlatform.toLowerCase().indexOf('win') !== -1) {
-                            d = d.replace('/', '\\');
+                        var pathArr = sourcePath.replace(/\/$/, '').split('/');
+
+                        var projectName = pathArr[pathArr.length - 1];
+
+                        var relativePath = filepath.split(projectName + path.sep)[1];
+                        var finalRemotePath = normalizePath(path.join(remotePath, relativePath));
+
+                        var dirname = path.dirname(finalRemotePath);
+
+                        var fileDirs = parents(dirname)
+                            .map(function (d) {
+                                return d.replace(/^\/~/, "~");
+                            })
+                            .map(normalizePath);
+
+                        if (dirname.search(/^\//) === 0) {
+                            fileDirs = fileDirs.map(function (dir) {
+                                if (dir.search(/^\//) === 0) {
+                                    return dir;
+                                }
+                                return '/' + dir;
+                            });
                         }
 
-                        process.stdout.write("\nmkdir " + chalk.gray(d));
-                        sftp.mkdir(d, {
-                            mode: '0755'
+                        fileDirs = fileDirs.filter(function (d) {
+                            return d.length >= remotePath.length && !mkDirCache[d];
+                        });
+
+                        async.whilst(function () {
+                            return fileDirs && fileDirs.length;
+                        }, function (next) {
+                            var d = fileDirs.pop();
+                            mkDirCache[d] = true;
+
+                            if (remotePlatform && remotePlatform.toLowerCase().indexOf('win') !== -1) {
+                                d = d.replace('/', '\\');
+                            }
+
+                            deployLog("\nCreando directorio " + chalk.gray(d));
+                            sftp.mkdir(d, {
+                                mode: '0755'
+                            }, function () {
+                                deployLog(" " + chalk.green('\u2714'));
+                                next();
+                            });
                         }, function () {
-                            process.stdout.write(" " + chalk.green('\u2714'));
-                            next();
-                        });
-                    }, function () {
-                        var fileHash = options.cacheFile && md5File.sync(filepath);
-                        if (fileHash && cache[finalRemotePath] === fileHash) {
-                            process.stdout.write("\n" + chalk.cyan("\u27A4 ") + chalk.gray(filepath) + chalk.white(" file was uploaded last time and haven't changed since then, skipping upload."));
-                            done();
-                            return;
-                        }
-                        var readStream = fs.createReadStream(filepath);
-
-                        var stream = sftp.createWriteStream(finalRemotePath, {
-                            flags: 'w',
-                            encoding: null,
-                            mode: '0666',
-                            autoClose: true
-                        });
-                        process.stdout.write("\n" + chalk.cyan("\u27A4 ") + chalk.gray(filepath) + chalk.white(" -> ") + chalk.gray(finalRemotePath));
-                        readStream.pipe(stream);
-
-                        stream.on('close', function (err) {
-
-                            if (err) {
-                                throw new Error('sftp2', err);
-                            } else {
-                                process.stdout.write(" " + chalk.green('\u2714'));
-                                fileCount++;
-                                cache[finalRemotePath] = fileHash;
+                            var fileHash = options.cacheFile && md5File.sync(filepath);
+                            if (fileHash && cache[finalRemotePath] === fileHash) {
+                                deployLog("\n" + chalk.cyan("\u27A4 ") + chalk.gray(filepath) + chalk.white(" el archivo se cargó la última vez y no ha cambiado desde entonces, se omite la carga."));
+                                done();
+                                return;
                             }
-                            done();
+                            var readStream = fs.createReadStream(filepath);
+
+                            var stream = sftp.createWriteStream(finalRemotePath, {
+                                flags: 'w',
+                                encoding: null,
+                                mode: '0666',
+                                autoClose: true
+                            });
+                            deployLog("\n" + chalk.cyan("\u27A4 ") + chalk.gray(filepath) + chalk.white(" -> ") + chalk.gray(finalRemotePath));
+                            readStream.pipe(stream);
+
+                            stream.on('close', function (err) {
+
+                                if (err) {
+                                    throw new Error('sftp2', err);
+                                    eventEmitter.emit('error', err);
+                                } else {
+                                    deployLog(" " + chalk.green('\u2714'));
+                                    fileCount++;
+                                    cache[finalRemotePath] = fileHash;
+                                }
+                                done();
+                            });
+
                         });
 
-                    });
-
-                }, function () {
-                    console.log('\nsftp2:', chalk.green(fileCount, fileCount === 1 ? 'archivo' : 'archivos', 'subidos correctamente'));
-                    finished = true;
-                    if (cacheFilePath) {
-                        fs.writeFileSync(cacheFilePath, JSON.stringify(cache));
-                        process.stdout.write("\narchivo caché " + cacheFilePath + "actualizado");
-                    }
-                    if (sftp) {
-                        sftp.end();
-                    }
-                    if (connectionCache) {
-                        connectionCache.end();
-                    }
-                    if (callback) {
-                        callback();
-                    }
-                    resolve();
-                });
-            });
-        }
-
-        function connectSftp(callback) {
-            console.log('Autenticando con contraseña.');
-
-            var c = new Connection();
-            connectionCache = c;
-            c.on('ready', function () {
-                c.sftp(function (err, sftp) {
-                    if (err) {
-                        throw err;
-                    }
-                    sftp.on('end', function () {
-                        console.log('Sesión SFTP cerrada');
-                        sftp = null;
-                        if (!finished) {
-                            console.log('error', new Error('sftp2', "Cierre de sesión SFTP inesperado"));
+                    }, function () {
+                        deployLog('\nsftp2:', chalk.green(fileCount, fileCount === 1 ? 'archivo' : 'archivos', 'subidos correctamente'));
+                        finished = true;
+                        if (cacheFilePath) {
+                            fs.writeFileSync(cacheFilePath, JSON.stringify(cache));
+                            deployLog("\narchivo caché " + cacheFilePath + "actualizado");
                         }
+                        if (sftp) {
+                            sftp.end();
+                        }
+                        if (connectionCache) {
+                            connectionCache.end();
+                        }
+                        if (callback) {
+                            callback();
+                        }
+                        resolve();
                     });
-                    console.log(chalk.green("Conectado"));
-                    callback(sftp);
+                });
+            }
+
+            function connectSftp(callback) {
+                deployLog('Autenticando con contraseña.');
+
+                var c = new Connection();
+                connectionCache = c;
+                c.on('ready', function () {
+                    c.sftp(function (err, sftp) {
+                        if (err) {
+                            eventEmitter.emit('error', err);
+                            throw err;
+                        }
+                        sftp.on('end', function () {
+                            deployLog('Sesión SFTP cerrada');
+                            sftp = null;
+                            if (!finished) {
+                                deployLog('error', new Error('sftp2', "Cierre de sesión SFTP inesperado"));
+                            }
+                        });
+                        deployLog(chalk.green("Conectado"));
+                        eventEmitter.emit('connected');
+                        callback(sftp);
+                    });
+
+                });
+                c.on('error', function (err) {
+                    deployLog('sftp2', err);
+                    eventEmitter.emit('error,');
+                    reject(err);
+                });
+                c.on('end', function () {
+                    // deployLog('Connection :: end');
+                });
+                c.on('close', function (hadError) {
+                    if (!finished) {
+                        deployLog('sftp2', "SFTP Cierre inesperado");
+                    }
+                    deployLog('Conexión :: cerrada', hadError ? "con error" : "");
+
                 });
 
-            });
-            c.on('error', function (err) {
-                console.log('sftp2', err);
-                reject(err);
-            });
-            c.on('end', function () {
-                // console.log('Connection :: end');
-            });
-            c.on('close', function (hadError) {
-                if (!finished) {
-                    console.log('sftp2', "SFTP Cierre inesperado");
+                /*
+                 * connection options, may be a key
+                 */
+                var connectionOptions = {
+                    host: options.host,
+                    port: options.port || 22,
+                    username: options.username
+                };
+
+                if (options.password) {
+                    connectionOptions.password = options.password;
                 }
-                console.log('Conexión :: cerrada', hadError ? "con error" : "");
 
-            });
-
-            /*
-             * connection options, may be a key
-             */
-            var connectionOptions = {
-                host: options.host,
-                port: options.port || 22,
-                username: options.username
-            };
-
-            if (options.password) {
-                connectionOptions.password = options.password;
+                if (options.timeout) {
+                    connectionOptions.readyTimeout = options.timeout;
+                }
+                c.connect(connectionOptions);
             }
 
-            if (options.timeout) {
-                connectionOptions.readyTimeout = options.timeout;
-            }
-            c.connect(connectionOptions);
-        }
+        });
 
-    });
+    };
+}
 
-};
+module.exports = SftpDeployer;
